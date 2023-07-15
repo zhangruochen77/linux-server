f579f0c04eb983559e0173ff3355c5d2b66ce366
zhangruochen77 <924945268@qq.com>
2023-07-15 22:36:33 +0800

    add: 添加cmake 文件以及文件连接方式

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/server/log/log.cc.o"
+  "CMakeFiles/server.dir/server/utils/utils.cc.o"
+  "../lib/libserver.pdb"
+  "../lib/libserver.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/test_log.cc
@@ -0,0 +1,7 @@
+#include "server/log/log.h"
+#include "server/utils/singleton.h"
+#include "server/utils/singleton.h"
+
+int main() {
+    std::cout << "hello" << std::endl;
+}
